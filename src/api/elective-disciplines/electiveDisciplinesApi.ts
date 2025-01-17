017193cbb237e47506ca10a1351e8a75aef89eb7
Andrei-David Nan <nandavid190@gmail.com>
2025-01-17 10:32:11 +0200

    chore: small fix regarding getElectivePacketsQuery in electiveDisciplinesApi

--- src/api/elective-disciplines/electiveDisciplinesApi.ts
+++ src/api/elective-disciplines/electiveDisciplinesApi.ts
@@ -1,7 +1,17 @@
+import { Discipline, DisciplinePacket, EnrollmentPeriod } from "../../types/disciplines/disciplines.types";
+
 import { API_URLS } from "../../config/api-config";
-import { EnrollmentPeriod } from "../../types/disciplines/disciplines.types";
 import { createApi } from "@reduxjs/toolkit/query/react";
 import getFetchBaseQuery from "../fetch-base-query";
+
+interface PacketWithDisciplines {
+  packet: DisciplinePacket;
+  disciplines: Discipline[];
+}
+
+interface PacketsResponse {
+  packets: PacketWithDisciplines[];
+}
 
 export const electiveDisciplinesApi = createApi({
   reducerPath: 'electiveDisciplinesApi',
@@ -10,11 +20,21 @@ export const electiveDisciplinesApi = createApi({
   endpoints: (builder) => ({
     getElectivePeriod: builder.query<EnrollmentPeriod, string>({
       query: (periodId) => ({
-        url: `${API_URLS.ENROLLMENT_PERIODS}/${periodId}${API_URLS.ELECTIVE_DISCIPLINES}`,
+        url: `${API_URLS.ENROLLMENT_PERIODS}/${periodId}/elective-disciplines`,
         method: 'GET',
       }),
       providesTags: (result, error, periodId) => [
         { type: 'EnrollmentPeriod', id: periodId }
+      ],
+    }),
+
+    getElectivePackets: builder.query<PacketsResponse, string>({
+      query: (periodId) => ({
+        url: `${API_URLS.ENROLLMENT_PERIODS}/${periodId}/packets`,
+        method: 'GET',
+      }),
+      providesTags: (result, error, periodId) => [
+        { type: 'ElectiveDisciplines', id: periodId }
       ],
     }),
   }),
@@ -22,4 +42,5 @@ export const electiveDisciplinesApi = createApi({
 
 export const {
   useGetElectivePeriodQuery,
+  useGetElectivePacketsQuery,
 } = electiveDisciplinesApi;
